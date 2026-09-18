b8eee25ac6205b91e3d2e3637e8d19804cba12c1
Xame14 <Arcedamian2006@gmail.com>
2026-09-18 17:56:56 -0500

    Refactor linked list printing and structure
    Refactored the main function to streamline the printing of the linked list and added a Print function to display the structure.

--- Lab 3 Link list.cpp
+++ Lab 3 Link list.cpp
@@ -1,23 +1,21 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
-
-
-
+void Print(int c, int p[], char l[], int li[]);
 
 int main()
 {
     int position[7];
-    
     char list[7];
-    
     int link[7];
-    
     int headpointer = 0;
     
     position[0] = 0;
     list[0] = 'D';
     link[0] = 99;
+    int counter = 1;
+    Print(counter, position, list, link);
     
     position[1] = 1;
     list[1] = 'B';
@@ -26,6 +24,8 @@ int main()
     // B should link to D
     
     link[1] = 0;
+    counter++;
+    Print(counter, position, list, link);
     
     position[2] = 2;
     list[2] = 'J';
@@ -34,6 +34,8 @@ int main()
     
     link[0] = 2;
     link[2] = 99;
+    counter++;
+    Print(counter, position, list, link);
     
     position[3] = 3;
     list[3] = 'M';
@@ -41,6 +43,8 @@ int main()
     // M > J 
     link[2] = 3;
     link[3] = 99;
+    counter++;
+    Print(counter, position, list, link);
     
     position[4] = 4;
     list[4] = 'F';
@@ -51,6 +55,8 @@ int main()
     
     link[0] = 4;
     link[4] = 2;
+    counter++;
+    Print(counter, position, list, link);
     
     position[5] = 5;
     list[5] = 'C'; 
@@ -63,6 +69,8 @@ int main()
     
     link[1] = 5;
     link[5] = 0;
+    counter++;
+    Print(counter, position, list, link);
     
     position[6] = 6;
     list[6] = 'W';
@@ -71,12 +79,20 @@ int main()
     
     link[3] = 6;
     link[6] = 99;
-    
-    for(int i = 0; i < 7; i++){
-        cout<<position[i] << "    " << list[i] << "    "<< link[i] << endl;
-    }
-    
-    
-    
+    counter++;
+    Print(counter, position, list, link);
 
     return 0;
+}
+
+void Print(int c, int p[], char l[], int li[] ){
+    
+    cout<<setfill('-')<<setw(20)<<' '<< setfill(' ')<<endl;
+    cout<<"Position list link" << endl;
+    for(int i = 0; i < c; i++){
+        cout<<right<<setw(5)<< p[i] <<left<<setw(5) <<' '
+            <<right<<setw(2)<< l[i] << left<<setw(4)<< ' '
+            <<left<<setw(1)<< li[i] <<right<<setw(1)<< endl;
+    }
+    cout<<setfill('-')<<setw(20)<<' '<< setfill(' ')<<endl<<endl<<endl;
+}
