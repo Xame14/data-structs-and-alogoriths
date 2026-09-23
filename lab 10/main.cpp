790c90831e9352083d3395833dc5cbd6e209f28a
Xame14 <Arcedamian2006@gmail.com>
2026-09-23 14:29:16 -0500

    Implement basic user interaction in main.cpp
    Fix syntax errors and improve user prompt.

---
+++ lab 10/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    string ans
+    cout<<"Hello to sort-o-matic!"<<endl<<"which alogoriths would you like to do"<<endl
+        <<"A: Insertion"<<endl
+        <<"B: Selction"<<endl
+        <<"C: Merge"<<endl
+        <<"D: Bubble"<<endl
+        <<endl<<"Your answer: ";
+    cin(getline, ans);
+
+    return 0;
+}
